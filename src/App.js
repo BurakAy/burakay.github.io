1c736cbba0e5f5044b5202add57b50007932c418
Burak Aydemir <baydemi@wgu.edu>
2024-05-21 13:46:12 -0700

    Added Links component to header element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Links from './components/Links';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        <Links />
       </header>
     </div>
   );
