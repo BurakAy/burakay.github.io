fb7bb10d177761b4cae9817099e0d236d01c32c5
Burak Aydemir <baydemi@wgu.edu>
2024-05-21 12:54:58 -0700

    Created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import '../styles/Header.css'
+
+function Header() {
+  return (
+    <>
+      <h1 id="header_title">Burak Aydemir</h1>
+      <h4 id="header_subtitle">Software Engineer | UX / Front End</h4>
+    </>
+  );
+}
+
+export default Header;
