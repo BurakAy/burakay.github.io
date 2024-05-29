9317843c697a14d623fbc21bcfac09c933721740
Burak Aydemir <baydemi@wgu.edu>
2024-05-29 07:58:23 -0700

    Refactored Panel component implementation

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Links from './components/Links';
 import Weather from './components/Weather';
 import Panel from './components/Panel';
 import Footer from './components/Footer';
+import skills from './assets/skills.json'
+import { useState } from 'react'
 
 function App() {
+  const [technicalSkills, setTechSkills] = useState(skills.tech_skills);
+  const [softSkills, setSoftSkills] = useState(skills.soft_skills);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,7 +18,10 @@ function App() {
         <Links />
       </header>
       <Weather />
-      <Panel panelTitle={'skills'}/>
+      <div className='App-skill-panels'>
+        <Panel panelTitle={'technical skills'} tiles={technicalSkills} />
+        <Panel panelTitle={'soft skills'} tiles={softSkills} />
+      </div>
       <Footer />
     </div>
   );
