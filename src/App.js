bffaca95c857bb6a0ba7edc76909c82a57243021
Burak Aydemir <baydemi@wgu.edu>
2024-05-28 17:48:36 -0700

    Added Panel and Footer components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './components/Header';
 import Links from './components/Links';
 import Weather from './components/Weather';
+import Panel from './components/Panel';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Links />
       </header>
       <Weather />
+      <Panel panelTitle={'skills'}/>
+      <Footer />
     </div>
   );
 }
