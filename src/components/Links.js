c5ae6fb4c9807b6a5e069385748fbcf53d91e079
Burak Aydemir <baydemi@wgu.edu>
2024-05-29 10:39:40 -0700

    Implemented logic for changing GitHub logo when dark mode preferred

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,11 +1,28 @@
 import '../styles/Links.css'
-import githubLogo from '../assets/github-icons/github-mark-white.svg'
+import githubLogoDark from '../assets/github-icons/github-mark-white.svg'
+import githubLogo from '../assets/github-icons/github-mark.svg'
 import linkedinLogo from '../assets/linkedin-icons/LI-In-Bug.svg'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function Links() {
+  const [gitLogo, setGitLogo] = useState(githubLogo)
+
+  const isDarkMode = () => {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)')) {
+      setGitLogo(githubLogoDark);
+    } else {
+      setGitLogo(githubLogo);
+    }
+  }
+
+  useEffect(() => {
+    isDarkMode();
+  }, [gitLogo]);
+
   return (
     <div className='social-logos'>
-      <a href='https://www.github.com/burakay' target='_blank' rel="noreferrer"><img id="github-logo" src={githubLogo} alt='GitHub Logo' /></a>
+      <a href='https://www.github.com/burakay' target='_blank' rel="noreferrer"><img id="github-logo" src={gitLogo} alt='GitHub Logo' /></a>
       <a href='https://www.linkedin.com/in/aydemirburak' target='_blank' rel="noreferrer"><img id='linkedin-logo' src={linkedinLogo} alt='LinkedIn Logo' /></a>
     </div>
   );
