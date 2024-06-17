3ca7bf7b89bf8024f33edb2fb581fc41d74fa7eb
Burak <14030652+BurakAy@users.noreply.github.com>
2024-06-17 13:50:29 -0700

    Created Featured component for displaying a featured project

---
+++ src/components/Featured.js
@@ -0,0 +1,48 @@
+import "../styles/Featured.css";
+
+function Featured() {
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const d = new Date();
+  const currDate = month[d.getMonth()] + " " + d.getFullYear();
+  return (
+    <div className="featured-wrapper">
+      <h3 className="section-title">Featured Project - {currDate}</h3>
+      <p>
+        Site Data Scraper{" "}
+        <a
+          className="app-link"
+          href="https://sitescraper.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          sitescraper.netlify.app
+        </a>
+      </p>
+      <p>
+        Technologies used: React, Express/Node, JavaScript, Playwright, Netlify,
+        CSS
+      </p>
+      <p className="featured__app-description">
+        Site Data Scraper allows users to scrape textual data from a user
+        defined URL. Users can label the data and pass in the selectors of the
+        elements containing the data to be scraped. Once the data has been
+        scraped, results are displayed to the user.
+      </p>
+    </div>
+  );
+}
+
+export default Featured;
