2ce214449b4b07b149f2a2ebebcaada94d1ccbbd
Burak <14030652+BurakAy@users.noreply.github.com>
2024-06-17 13:52:19 -0700

    Automatically formatted

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
-import '../styles/Weather.css'
+import "../styles/Weather.css";
 
 function Weather() {
-  const [location, setLocation] = useState({ city: '', state: '' });
+  const [location, setLocation] = useState({ city: "", state: "" });
   const [temperature, setTemperature] = useState();
 
   // calling this endpoint to get a user's location information
   // city, state, latitude, longitude
   const getLocation = () => {
-    const locationAPI = "https://geolocation-db.com/json/"
+    const locationAPI = "https://geolocation-db.com/json/";
     fetch(locationAPI)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setLocation({ city: data.city, state: data.state });
         getWeather(data.latitude, data.longitude);
       });
@@ -22,32 +22,34 @@ function Weather() {
   const getWeather = (latitude, longitude) => {
     const weatherAPI = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch`;
     fetch(weatherAPI)
-      .then(response => response.json())
-      .then(data => setTemperature(data.current.temperature_2m))
+      .then((response) => response.json())
+      .then((data) => setTemperature(data.current.temperature_2m));
   };
 
   // making sure the APIs are only called on the first render
   // by passing in an empty array as the second parameter
   useEffect(() => {
     getLocation();
-  }, [])
+  }, []);
 
   // only displaying the weather widget if temperature value has been set
   const displayWeatherWidget = () => {
-    const widget = 
-    <div className="weather-container">
-      <p id="location">{location.city}, {location.state }</p>
-      <p id="temperature">Current Temp: <span id="temperature_value">{ temperature } &#8457;</span></p>
-    </div>
+    const widget = (
+      <div className="weather-container">
+        <p id="location">
+          {location.city}, {location.state}
+        </p>
+        <p id="temperature">
+          Current Temp:{" "}
+          <span id="temperature_value">{temperature} &#8457;</span>
+        </p>
+      </div>
+    );
 
-    return temperature ? widget : null; 
-  }
+    return temperature ? widget : null;
+  };
 
-  return (
-    <>
-    {displayWeatherWidget()}
-    </>
-  );
+  return <>{displayWeatherWidget()}</>;
 }
 
 export default Weather;
