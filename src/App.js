6255c1b5bdc9e70daa1b2a68900fc03dce7a9f3e
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-29 19:56:39 -0700

    Projects object props

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Weather from "./components/Weather";
 import Panel from "./components/Panel";
 import Footer from "./components/Footer";
 import skills from "./assets/skills.json";
+import projects from "./assets/projects.json";
 import Featured from "./components/Featured";
 import { useState } from "react";
 
 function App() {
   const [technicalSkills, setTechSkills] = useState(skills.tech_skills);
   const [softSkills, setSoftSkills] = useState(skills.soft_skills);
+  const [techProjects, setProjects] = useState(projects.techProjects);
 
   return (
     <div className="App">
@@ -20,7 +22,7 @@ function App() {
       </header>
       <Weather />
       <div className="App-panels">
-        <Featured />
+        <Featured techProjects={techProjects} />
       </div>
       <div className="App-panels">
         <Panel panelTitle={"technical skills"} tiles={technicalSkills} />
